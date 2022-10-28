5797de69a356e0b5451f3b77303e9fd32be5bded
Daniel <studiosdopplermusic@gmail.com>
2022-10-28 07:57:39 -0500

    app

---
+++ Notes/API/01_WTF_is_API.js
@@ -0,0 +1,26 @@
+/*
+
+API
+
+Pieza de software que sirve para la comunicación de un software con otro
+
+
+ARQUITECTURA REST (Representational States Transfer)
+
+- Forma de construir un API de manera lógica haciendolas faciles de consumir
+
+- Principios
+-- Separar en recursos logicos
+-- Estar disponibles mediante URLs
+-- Usar http methods y no la URL 
+-- Los datos deben de usar el formato JSON
+-- Deben de ser rápidas
+
+
+
+NOTAS:
+- EndPoint: Punto de acceso a información dentro del API
+-
+
+
+*/ 
