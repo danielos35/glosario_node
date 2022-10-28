72c1caae639b4412d67970a319f2adc5c544c9c0
Daniel <studiosdopplermusic@gmail.com>
2022-10-27 21:55:40 -0500

    basic routing

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', ( req, res ) => {
-    res.status(200).send('Hello');
-    console.log(req);
+    res.status(200).json({data:'Hello word'});
 });
+
+
+app.post('/', ( req, res ) => {
+    res.send('You can post to this endpoint...')
+})
 
 const port = 3000;
 app.listen(port, (res)=>{
